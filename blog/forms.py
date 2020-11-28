08f3fd2f4383aa7010d8c7bcf903e70683c0d818
liuguangjiu <wo893569268@163.com>
2020-11-28 06:41:42 -0800

    gerenboke

---
+++ blog/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from comments.models import Comments
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = ['name', 'email', 'url', 'content']
